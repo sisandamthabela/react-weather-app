28f3f731c6eeb76846d9f539c4d0144071af90a2
sisandamthabela <mthabelasisanda@gmail.com>
2024-06-22 12:59:31 +0200

    add github repo url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/sisandamthabela"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sisanda Mthabela
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/sisandamthabela/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
