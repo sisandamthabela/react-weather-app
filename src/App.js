fc599e140c4e23f391f90a28a4ee982107b5d2b4
sisandamthabela <mthabelasisanda@gmail.com>
2024-06-22 13:13:40 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/sisandamthabela"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Sisanda Mthabela
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/sisandamthabela/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/sisandamthabela"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sisanda Mthabela
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/sisandamthabela/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
